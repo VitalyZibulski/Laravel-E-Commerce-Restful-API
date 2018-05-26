ba696b3239ef809ca4e10c5a9cba5be9dbca643b
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-05-26 11:35:11 +0300

    finish project API

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -41,6 +41,7 @@ class ReviewController extends Controller
     {
         $review = new Review($request->all());
         $product->reviews()->save($review);
+
         return response([
         	'data' => new ReviewResource($review)
         ], Response::HTTP_CREATED);
@@ -75,9 +76,12 @@ class ReviewController extends Controller
      * @param  \App\Models\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Review $review)
+    public function update(ReviewRequest $request, Product $product, Review $review)
     {
-        //
+		$review->update($request->all());
+		return response([
+			'data' => new ReviewResource($review)
+		], Response::HTTP_CREATED);
     }
 
     /**
@@ -86,8 +90,10 @@ class ReviewController extends Controller
      * @param  \App\Models\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Review $review)
+    public function destroy(Product $product,Review $review)
     {
-        //
+        $review->delete();
+
+		return response(null,Response::HTTP_NO_CONTENT);
     }
 }
